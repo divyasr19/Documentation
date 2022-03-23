f2f70927766ed95dd52d4a356952a850c670cbcc
divyasr19 <101968370+divyasr19@users.noreply.github.com>
2022-03-23 21:38:15 +0530

    Add files via upload
    OOPS documentation

---
+++ task26.py
@@ -0,0 +1,47 @@
+#OOPS:
+'''
+oops stands for object oriented programming language
+Oops is based on objects not on functions
+oops follows bottom up approch
+oops provides data security
+modificatin can be done easily
+it uses access specifiers
+
+ex:java,cpp,python,...
+
+FEATURES:(6)
+1.CLASS:
+  a blue print of an object
+  it does not exist physically
+  it cantains variables and methds
+ex:animal
+
+2.OBJECT;
+  it is an instance of class
+  ex:dog,cat,tiger
+
+3.INHERITANCE:
+ the process of creating a new class from the existing class
+ is called inheritance
+
+ the exisisting class is called PARENT or SUPER or BASE class
+ the new class is called SUB class or CHILD class or DERIVED class
+
+ inheritance is of 4 types:
+ a)single:1 parent 1 child
+ b)multiple:child is deriving frm 2 parent cls
+ c)multilevel:parent-->child
+                       parent-->child
+ d)heirarchial:
+ 1 parent 2 child cls
+
+ 4.POLYMORPHISM:
+ the process of exisisting more than one forms
+
+ 5.ENCAPSULATION:
+ wrapping of vars and methods
+
+ 6.DATA ABSTRACTION:
+   hiding some information and gives only require dvalues
+
+'''
