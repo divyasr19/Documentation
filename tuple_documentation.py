6e83514b648f0115c245f187031b26c86e77ec46
divyasr19 <101968370+divyasr19@users.noreply.github.com>
2022-03-23 21:36:30 +0530

    Add files via upload
    Tuple documentation

---
+++ tuple_documentation.py
@@ -0,0 +1,19 @@
+'''#tuple
+these are immutable
+it is similar to list
+these are enclosed with()
+separated by ,
+tuples are immtable so it don't have methods
+but it has some inbuiilt fun
+1.len:returns len of tuple
+2.max:returns max value in the tuple
+3.min:returns min value in the tuple
+4.sum:returns sum of tuple 
+5.sorted:returns sorted  tuple
+
+in and not in:
+to check whether the ele is present or not
+is and is not :
+checks the addrs of elementss 
+
+'''
