dd5076bcac9592d9e296b3e8a2b9a4a5f29b17c3
divyasr19 <101968370+divyasr19@users.noreply.github.com>
2022-03-23 21:35:25 +0530

    Add files via upload
    Strings and its methods

---
+++ task12.py
@@ -0,0 +1,77 @@
+#strings
+'''
+string is a collection of character enclosed either with single or double or triple quotes
+
+string may contain alphanumerics also
+indexing is same as list'
+slicing also same as list
+
+
+string methods
+1.capitalize:
+returns first letter as capital and remaining are lower case
+2.upper:
+returns the str in upper caes
+3.lower:
+returns the str in lower case
+4.center:
+it takes 2 parameters namely width and value
+this alligns the str at center with specified witdh the remaing
+place is filled by value
+
+5.count()
+it returns no. of times the sub str or str is repeated
+
+6.startswith:
+it returns  True if the str var starts with str or substr
+o.w  False
+ 
+ 
+ 7.endswith:
+ returns true if the strvar ends with specified substr or str
+ o.w False
+ 
+ 8.format
+ it is useful when we want to edit only partcular vaue
+ 
+ 9.find:
+ returns the first index of particular str
+ 
+ 10.index():
+ returns the first index of particular str
+ 
+11.isalnum
+returns true if the str either alpahbets or numerics ow.false
+
+12.isdecimal
+returns True if str has int
+ow false
+13.isdigit()
+returns True if the str is int
+14.split()
+converts str into list
+
+15.join;
+joins particular value at space given by split fun
+
+16.lstrip
+removes the left spaces and returs the remaing str
+ 
+17.strip
+removes the right spaces and returs the remaing str
+
+18.strip
+removes the both left  and right spaces and returs the remaing str
+
+19.title()
+returns every word starts with capital
+
+20removeprefix()
+removes the sub str from front side 
+
+21removesuffix()
+removes the sub str from back side 
+
+22.replace()
+replaces old value with new value
+ '''
