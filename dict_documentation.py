683d5c91e050340b2672feaa8dbd1593a1a49007
divyasr19 <101968370+divyasr19@users.noreply.github.com>
2022-03-28 19:21:28 +0530

    Add files via upload

---
+++ dict_documentation.py
@@ -0,0 +1,51 @@
+"""
+dictinary:
+it is a key value pair
+these are mutable
+dicts are ordered
+key acts as index
+key can be either str or int or float
+keys are unique
+values may be duplicate
+values may be int or float or str or list or tuple or dict
+
+syntax:
+dict_var={key1:"value1",key2:"value2, ...}
+
+we can access value by key i.e
+dict_var[key]
+we can  give multiple bvalues to key
+dict_var={key1:value1,key2:value3,value2,...}
+to access value 2
+dict_var[key2][1] 
+
+we can access one key at atime
+#methods:
+1.clear:
+return emty dict
+
+2.copy()
+copies one dict into other dict and no operation is updated after copy meyhod
+
+3.get()
+we pass key into get(),to get value
+
+4.items():
+it return both keys and values
+
+5.keys():
+returns keys only
+
+6.values()
+returns values only
+
+7.update
+appeds key pair at the end of dict
+
+8.pop
+removes values belonging to particular key
+
+
+we can create nested list also
+
+"""
