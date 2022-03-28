8d7e8d62122bca4729c94d30aa3ec4ab3e8bc9f2
divyasr19 <101968370+divyasr19@users.noreply.github.com>
2022-03-28 19:20:50 +0530

    Add files via upload

---
+++ list_documentation.py
@@ -0,0 +1,38 @@
+'''
+list:
+list can able to store different datatypes
+lists are mutable,i.e,we can modify whenever we want
+lists are enclosed by[] and seperated by ","
+The items in list are identified by index
+It allows duplication of elements
+the starting index is 0 and ending index is n-1
+structure of list:
+list_var=[ele,ele1,ele,.....]
+lists have negative index also
+len-no. of elements in the list
+slicing gives a piece of list,it can be done by :
+  list_var[start:stop:step] 
+we can create list within list which is nested list
+
+
+list methods:
+append:adds the element at the end 
+extend:we can pass multiple values as list
+
+Difference between append and extend:
+append considers list values as single elmemt
+extend considers list values as individual element
+
+copy:it copies the list and returns it,after copy operation we cannot update anything
+clear:it removes all elements from list
+count:it returns how many times the vlue is 
+index:it returns index of particular value
+insert:it inserts value at desired positin
+pop: it removes elemt at given index
+remove:it removes  ele whatever value we have given
+reverse:it returns total list in the reverse order
+sort: it returns total list in ascending order
+
+
+
+'''
